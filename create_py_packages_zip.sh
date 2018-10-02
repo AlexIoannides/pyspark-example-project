0cdc4bd49d451dc138fd2a1427a10068ee9cdcb4
alexioannides <alex.ioanndies@yahoo.co.uk>
2018-10-02 21:13:51 +0100

    code changes prior to testing

---
+++ create_py_packages_zip.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# check to see if pipenv is installed
+if [ -x "$(which pipenv)" ]
+then
+    # check that there Pipfile exists in root directory
+    if [ ! -e Pipfile ]
+    then
+        echo 'ERROR - cannot find Pipfile'
+        exit 1
+    fi
+
+    # use Pipenv to create a requirement.txt file
+    echo '... creating requirements.txt from Pipfile'
+    pipenv lock -r > requirements.txt
+
+    # install packages to a temporary directory and zip it
+    pip3 install -r requirements.txt --target ./packages
+
+    # check to see if there are any external dependencies
+    # if not then create an empty file to see zip with
+    if [ -z "$(ls -A packages)" ]
+    then
+        touch packages/empty.txt
+    fi
+
+    # zip dependencies
+    if [ ! -d packages ]
+    then 
+        echo 'ERROR - pip failed to import dependencies'
+        exit 1
+    fi
+
+    cd packages
+    zip -9mrv packages.zip .
+    mv packages.zip ..
+    cd ..
+
+    # remove temporary directory and requirements.txt
+    rm -rf packages
+    rm requirements.txt
+    
+    # add local modules
+    echo '... adding all modules from local utils package'
+    zip -ru9 packages.zip dependencies -x dependencies/__pycache__/\*
+
+    exit 0
+else
+    echo 'ERROR - pipenv is not installed --> run pip3 install pipenv to load pipenv into global site packages or install via a system package manager.'
+    exit 1
+fi
--- etl_job.py
+++ etl_job.py
@@ -11,7 +11,7 @@ example, this example script can be executed as follows,
 
     $SPARK_HOME/bin/spark-submit \
     --master local[*] \
-    --py-files dependencies.zip \
+    --py-files packages.zip \
     --files etl_config.json \
     etl_job.py
 
@@ -32,16 +32,14 @@ functions, such that the key Transform steps can be covered by tests
 and jobs or called from within another environment (e.g. a Jupyter or
 Zeppelin notebook).
 """
-# imports from Python Standard Library
+
 from os import listdir, path
 from json import loads
 
-# imports downloaded from PyPi
 from pyspark import SparkFiles
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import *
 
-# local dependencies
 from dependencies import logging
 
 
@@ -155,13 +153,13 @@ def create_test_data(spark, config):
 
 def start_spark(app_name='my_spark_app', master='local[*]', jar_packages=[],
                 files=[], spark_config={}):
-    """Start Spark session, get the Spark logger and load config files.
+    """Start Spark session, get Spark logger and load config files.
 
     Start a Spark session on the worker node and register the Spark
-    application with the cluster. NOTE - only the app_name argument
-    will apply when this is called from a script sent to spark-submit
-    (i.e. when __name__ = '__main__'). All other arguments exist solely
-    for testing the script from within an interactive Python console.
+    application with the cluster. Note, that only the app_name argument
+    will apply when this is called from a script sent to spark-submit. 
+    All other arguments exist solely for testing the script from within 
+    an interactive Python console.
 
     This function also looks for a file ending in 'config.json' that
     can be sent with the Spark job. If it is found, it is opened,
@@ -169,18 +167,34 @@ def start_spark(app_name='my_spark_app', master='local[*]', jar_packages=[],
     configuration), into a dict of ETL job configuration parameters,
     which are returned as the last element in the tuple returned by
     this function. If the file cannot be found then the return tuple
-    only contains the Spark session and Spark logger objects.
+    only contains the Spark session and Spark logger objects and None 
+    for config.
+
+    The function checks the enclosing environment to see if it is being
+    run from inside an interactive console session or from an 
+    environment which has a DEBUG environment varibale set (i.e. from 
+    within a DEBUG run with custom environment variables). In this 
+    scenario, the function uses all available function arguments to 
+    start a PySpark driver from the local PySpark package as opposed to
+    using the spark-submit and Spark cluster defaults. This will also 
+    use local module imports, as opposed to those in the zip archive 
+    sent to spark via the --py-files flag in spark-submit.
 
     :param app_name: Name of Spark app.
     :param master: Cluster connection details (defaults to local[*].
     :param jar_packages: List of Spark JAR package names.
     :param files: List of files to send to Spark cluster (master and
-    workers).
+        workers).
     :param spark_config: Dictionary of config key-value pairs.
     :return: A tuple of references to the Spark session, logger and
-    config dict (only if available).
+        config dict (only if available).
     """
-    if __name__ == '__main__':
+
+    # detect execution environment
+    flag_repl = False if hasattr(__main__, '__file__') else True
+    flag_debug = True if 'DEBUG' in environ.keys() else False
+
+    if not (flag_repl or flag_debug):
         # get Spark session factory
         spark_builder = (
             SparkSession
@@ -222,14 +236,10 @@ def start_spark(app_name='my_spark_app', master='local[*]', jar_packages=[],
         config_dict = loads(config_json)
         spark_logger.warn('loaded config from ' + config_files[0])
     else:
+        spark_logger.error('no config file found')
         config_dict = None
 
-    # build return tuple conditional on presence of config
-    if config_dict is not None:
-        return_tup = spark_sess, spark_logger, config_dict
-    else:
-        return_tup = spark_sess, spark_logger
-    return return_tup
+    return spark_sess, spark_logger, config_dict
 
 
 # entry point for PySpark ETL application
