6175a7923ee43d0d24c63983f8047c5e36f6164a
alexioannides <alex.ioanndies@yahoo.co.uk>
2019-02-19 11:12:58 +0000

    Enhancements to build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -3,22 +3,23 @@
 # check to see if pipenv is installed
 if [ -x "$(which pipenv)" ]
 then
-    # check that there Pipfile exists in root directory
-    if [ ! -e Pipfile ]
+    # check that Pipfile.lock exists in root directory
+    if [ ! -e Pipfile.lock ]
     then
-        echo 'ERROR - cannot find Pipfile'
+        echo 'ERROR - cannot find Pipfile.lock'
         exit 1
     fi
 
     # use Pipenv to create a requirement.txt file
-    echo '... creating requirements.txt from Pipfile'
+    echo '... creating requirements.txt from Pipfile.lock'
     pipenv lock -r > requirements.txt
 
     # install packages to a temporary directory and zip it
+    touch requirements.txt  # safeguard in case there are no packages
     pip3 install -r requirements.txt --target ./packages
 
     # check to see if there are any external dependencies
-    # if not then create an empty file to see zip with
+    # if not then create an empty file to seed zip with
     if [ -z "$(ls -A packages)" ]
     then
         touch packages/empty.txt
@@ -46,6 +47,6 @@ then
 
     exit 0
 else
-    echo 'ERROR - pipenv is not installed --> run pip3 install pipenv to load pipenv into global site packages or install via a system package manager.'
+    echo 'ERROR - pipenv is not installed --> run `pip3 install pipenv` to load pipenv into global site packages or install via a system package manager.'
     exit 1
 fi
